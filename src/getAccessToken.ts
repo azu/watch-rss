f4ddfd2db5973472ab6cc2f3c9f2aef80b275dd1
azu <azuciao@gmail.com>
2021-01-03 16:17:59 +0900

    update

--- src/getAccessToken.ts
+++ src/getAccessToken.ts
@@ -5,7 +5,10 @@ import { AuthorizationCode } from "simple-oauth2";
 import { OAuthConfig } from "./inoreader/OauthConfig";
 import open from "open";
 import readline from "readline";
+import { Octokit } from "@octokit/rest";
 import sodium from "tweetsodium";
+// @ts-ignore
+import nacl from "tweetnacl";
 
 function askQuestion(query: string): Promise<string> {
     const rl = readline.createInterface({
@@ -21,7 +24,34 @@ function askQuestion(query: string): Promise<string> {
     );
 }
 
+// https://github.com/maximilianMairinger/ctp/blob/95dbb0237c02169aa92edd4ad765204a4a44be11/src/app/project/app/app.ts
+// get repository public key →　update secrets
 async function run() {
+    const octokit = new Octokit({
+        auth: process.env.GITHUB_TOKEN!
+    });
+    const GITHUB_REPOSITORY = process.env.GITHUB_REPOSITORY;
+    if (!GITHUB_REPOSITORY) {
+        throw new Error("require GITHUB_REPOSITORY env");
+    }
+    const [owner, repo] = GITHUB_REPOSITORY.split("/");
+    const publicKey = await octokit.actions.getRepoPublicKey({
+        owner,
+        repo
+    });
+    const message = "xxx";
+    const textEncoder = new TextEncoder();
+    const encryptedBytes = sodium.seal(textEncoder.encode(message), Buffer.from(publicKey.data.key, "base64"));
+    const encrypted = Buffer.from(encryptedBytes).toString("base64");
+    console.log("encrypted", encrypted);
+    await octokit.actions.createOrUpdateRepoSecret({
+        owner: "azu",
+        repo: "watch-rss",
+        secret_name: "INOREADER_TOKENS",
+        encrypted_value: encrypted,
+        key_id: publicKey.data.key_id
+    });
+    return;
     const client = new AuthorizationCode(OAuthConfig);
     const authorizationUri = client.authorizeURL({
         redirect_uri: "http://localhost:3000/callback",
@@ -42,7 +72,7 @@ async function run() {
         console.log("Copy");
         /**
          *
-{
+         {
   token: {
     access_token: 'xxx',
     expires_in: 86400,
